48e1cd49079a5db9947a95351baf3f40fc1c0b99
renanarimon <renana1414@gmail.com>
2022-04-03 16:18:40 +0300

    comment

--- Matrix.hpp
+++ Matrix.hpp
@@ -8,40 +8,43 @@ namespace zich{
         std::vector<double> _mat;
         int _row;
         int _col;
-        friend std::ostream& operator<<(std::ostream&, const Matrix&);
-        friend std::istream & operator >> (std::istream &in,  Matrix& mat);
-        friend Matrix operator*(double scalar, const Matrix& A);
-        friend Matrix operator*=(double scalar, Matrix& A);
+
+        friend std::istream & operator >> (std::istream& in,  Matrix& mat); //cin
+        friend std::ostream& operator<<(std::ostream& out, const Matrix& mat); //cout
+        
+        friend Matrix operator*(double scalar, const Matrix& mat); //mat2 = scalar*mat1
+        friend Matrix operator*=(double scalar, Matrix& other); //mat1 = scalar*mat1
 
 
     public:
-        Matrix(std::vector<double> mat, int row, int col);
-        Matrix();
-        ~Matrix();
-        Matrix operator+(const Matrix& other);
-        Matrix operator+=(const Matrix& other);
-        Matrix operator+();
+        Matrix(std::vector<double> mat, int row, int col); //constructor
+        Matrix(); // empty constructor
+        ~Matrix(); //distructor
+        Matrix operator+(const Matrix& other); // mat = this+other
+        Matrix operator+=(const Matrix& other); // this = this+other
+        Matrix operator+(); // this = +(this)
 
-        Matrix operator-(const Matrix& other);
-        Matrix operator-=(const Matrix& other);
-        Matrix operator-();
+        Matrix operator-(const Matrix& other);// mat = this-other
+        Matrix operator-=(const Matrix& other);// this = this-other
+        Matrix operator-();// mat = -(this)
 
-        bool operator>(const Matrix& other);
-        bool operator<(const Matrix& other);
-        bool operator>=(const Matrix& other);
-        bool operator<=(const Matrix& other);
-        bool operator==(const Matrix& other);
-        bool operator!=(const Matrix& other);
 
-        Matrix operator++(); // prefix
-        Matrix operator++(int); // postfix
-        Matrix operator--(); // prefix
-        Matrix operator--(int); // postfix
+        bool operator>(const Matrix& other); //this > other --> true
+        bool operator<(const Matrix& other); //this < other --> true
+        bool operator>=(const Matrix& other); //this >= other --> true
+        bool operator<=(const Matrix& other); //this <= other --> true
+        bool operator==(const Matrix& other); //this == other --> true
+        bool operator!=(const Matrix& other); //this != other --> true
 
-        Matrix operator*(int scalar);
-        Matrix operator*(const Matrix& other);
-        Matrix operator*=(int scalar);
-        Matrix operator*=(const Matrix& other);
+        Matrix operator++(); // ++this
+        Matrix operator++(int); // this++
+        Matrix operator--(); // --this
+        Matrix operator--(int); // this--
+
+        Matrix operator*(double scalar); // mat = this*scalar
+        Matrix operator*(const Matrix& other); // mat = this*other
+        Matrix operator*=(double scalar); // this = this*scalar
+        Matrix operator*=(const Matrix& other); // this = this*other
 
     };
 
